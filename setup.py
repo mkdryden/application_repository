968e7977d1dd7cf4733aa034aff89727b92a7963
Christian Fobel <christian@fobel.net>
2014-02-07 08:34:31 -0500

    Restructure as Python package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+
+setup(name='application_repository',
+      version='0.0.1',
+      description='A Django site for serving versioned application and '
+      'plug-in downloads.',
+      keywords='application distribute plugin download django',
+      author='Christian Fobel',
+      url='http://github.com/cfobel/application_repository.git',
+      license='GPL',
+      packages=['application_repository',
+                'application_repository.application',
+                'application_repository.application.scripts',
+                'application_repository.plugins',
+                'application_repository.plugins.scripts',
+                'application_repository.repository'],
+      package_data={'application_repository': ['settings.py.skeleton',
+                                               'app_data/.empty_file',
+                                               'config/apache/*',
+                                               'config/postgresql/*',
+                                               'plugin_data/.empty_file'],
+                    'application_repository.plugins': ['*.skeleton']})
